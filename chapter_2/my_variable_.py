e5ad38ef78c6796cf1339dea0597560415d0f1ac
Ronny2001789 <ronaldmputla25@gmail.com>
2025-02-24 14:11:56 +0200

    publishpython

---
+++ chapter_2/my_variable_.py
@@ -0,0 +1,37 @@
+name = "ronny"
+
+surname = "mputla"
+
+print(name + surname)
+
+my_sentence = "the new world"
+
+print(my_sentence.title())
+
+
+my_initial = f"{name} {surname}"
+
+print(my_initial)
+
+
+my_name = "moronza"
+ 
+my_players= "dybala"
+
+my_car = "bmw"
+ 
+full_data = f"{my_name} {my_players} {my_car}"
+
+
+print(full_data)
+
+rules = "please attend everyday "
+ 
+print("\tmr david said " + rules  ) 
+
+print("good things take time.\nall people deserve the best.\n\tteachers always have time for students ")
+
+
+
+my_name,surname,my_car ="ronny","mputla","bmw"
+print(my_name + my_car)
