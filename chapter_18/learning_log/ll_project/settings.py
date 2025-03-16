80bd941770d474d3eb817632b2a85623db0fc9b5
Ronny2001789 <ronaldmputla25@gmail.com>
2025-03-16 23:20:10 +0200

    rggdf

--- chapter_18/learning_log/ll_project/settings.py
+++ chapter_18/learning_log/ll_project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #my app
+    'learning_logs',
+    #default django 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
