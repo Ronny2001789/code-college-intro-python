57a49a5d2a72e31c3df0cd80e0dadd4d905d5a61
Ronny2001789 <ronaldmputla25@gmail.com>
2025-03-18 07:57:47 +0200

    rg

--- chapter1/my_first_python.py
+++ chapter1/my_first_python.py
@@ -1,6 +1,9 @@
-print("hello world")
-#number 2/running django 
-& ll_env\Scripts\Activate
+# print("hello world")
+# #number 3/running django 
+# & ll_env\Scripts\Activate
 
-#####number1
-Set-ExecutionPolicy Unrestricted -Scope Process
+# #####number1
+# Set-ExecutionPolicy Unrestricted -Scope Process
+
+# ## number 2
+# python manage.py runserver
